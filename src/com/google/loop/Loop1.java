5cf9dbf862168c20cff20530fdbc537a099d6b40
vpfl95 <k_dh610@naver.com>
2022-07-07 17:49:30 +0900

    loop1

---
+++ src/com/google/loop/Loop1.java
@@ -0,0 +1,12 @@
+package com.google.loop;
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i =0; i<5; i++) {
+			System.out.println("Hello World");
+		}
+	}
+
+}
