e6708caa8ae5105d1eb5aac88cc58813a5665b59
vpfl95 <k_dh610@naver.com>
2022-08-03 11:09:16 +0900

    커넥션완료

--- src/main/java/com/iu/start/test/DBConnectionTest.java
+++ src/main/java/com/iu/start/test/DBConnectionTest.java
@@ -1,10 +1,20 @@
 package com.iu.start.test;
 
+import java.sql.Connection;
+
+import com.iu.start.util.DBConnector;
+
 public class DBConnectionTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			Connection con = DBConnector.getConnection();
+			System.out.println(con != null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
