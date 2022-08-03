40dbc19e01f9f4cdde92f3d60aa8ca97957d5ae4
vpfl95 <k_dh610@naver.com>
2022-08-03 10:40:14 +0900

    초기화

---
+++ src/main/java/com/iu/start/test/DBConnectionTest.java
@@ -0,0 +1,10 @@
+package com.iu.start.test;
+
+public class DBConnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/iu/start/test/TestMain.java
@@ -0,0 +1,10 @@
+package com.iu.start.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
