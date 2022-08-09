69ebf860873c04fc94ab96743d1c5858a32cbb1a
vpfl95 <k_dh610@naver.com>
2022-08-09 12:03:14 +0900

    1

--- src/main/java/com/iu/start/bankbook/BankBookDTO.java
+++ src/main/java/com/iu/start/bankbook/BankBookDTO.java
@@ -3,6 +3,32 @@ package com.iu.start.bankbook;
 public class BankBookDTO {
 	private int booknum;
 	private String bookname;
-	private int bookrate;
+	private double bookrate;
 	private int booksale;
+	public int getBooknum() {
+		return booknum;
+	}
+	public void setBooknum(int booknum) {
+		this.booknum = booknum;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public double getBookrate() {
+		return bookrate;
+	}
+	public void setBookrate(double bookrate) {
+		this.bookrate = bookrate;
+	}
+	public int getBooksale() {
+		return booksale;
+	}
+	public void setBooksale(int booksale) {
+		this.booksale = booksale;
+	}
+	
+	
 }
